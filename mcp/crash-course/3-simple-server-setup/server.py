31e47a246720d6b0bca961609d2c03b996e939f4
HRISHIKESH BADE <78660475+HBADE2002@users.noreply.github.com>
2025-09-07 22:21:01 +0530

    remove prints that caused issue to run mcp server

--- mcp/crash-course/3-simple-server-setup/server.py
+++ mcp/crash-course/3-simple-server-setup/server.py
@@ -23,13 +23,10 @@ def add(a: int, b: int) -> int:
 if __name__ == "__main__":
     transport = "stdio"
     if transport == "stdio":
-        print("Running server with stdio transport")
         mcp.run(transport="stdio")
     elif transport == "sse":
-        print("Running server with SSE transport")
         mcp.run(transport="sse")
     elif transport == "streamable-http":
-        print("Running server with Streamable HTTP transport")
         mcp.run(transport="streamable-http")
     else:
         raise ValueError(f"Unknown transport: {transport}")
